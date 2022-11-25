4aeb16f60e3b0e37e2a391f63ac19eb0713d28e3
masahiro96848 <feetwcpic9436@gmail.com>
2022-11-25 16:43:32 +0900

    タスクを追加する機能を追加

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,24 @@
         <section class="container-area">
             <div>
                 <input
+                    v-model="state.todo"
                     class="input-area"
                     placeholder="Todoを追加"
                     type="text"
                 />
+                <button @click="addTodo">追加</button>
             </div>
         </section>
         <!-- todoListのエリア -->
         <section class="container-area">
             <div>
-                <input
-                    class="input-area"
-                    placeholder="キーワードを検索"
-                    type="text"
-                />
-            </div>
-        </section>
-        <section class="container-area">
-            <div>
                 <ul class="list-row">
                     <li
-                        v-for="(todo, index) in todoList.todos"
+                        v-for="(todo, index) in state.todos"
                         :key="index"
                         class="list"
                     >
-                        <span class="item">{{ todo.title }}</span>
+                        <span class="item">{{ todo }}</span>
                         <i class="fa fa-trash" aria-hidden="true"></i>
                     </li>
                 </ul>
@@ -42,21 +35,19 @@ import { defineComponent, ref, reactive } from 'vue'
 
 export default defineComponent({
     setup() {
-        // const todoList = ref(['test1', 'test2', 'test3'])
-        const todoList = reactive({
+        const state = reactive({
             todo: '',
-            todos: [
-                {
-                    title: 'テストサンプル1',
-                },
-                {
-                    title: 'テストサンプル2',
-                },
-            ],
+            todos: [],
         })
 
+        const addTodo = () => {
+            state.todos.push(state.todo)
+            state.todo = ''
+        }
+
         return {
-            todoList,
+            state,
+            addTodo,
         }
     },
 })
