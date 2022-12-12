3b780e90ea9eb0c69729a7eefba4e65206b9b37f
masahiro96848 <feetwcpic9436@gmail.com>
2022-12-13 07:51:35 +0900

    feat: AddTodoコンポーネントを作成

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,35 @@
 <template>
     <div class="container">
         <h1 class="title">Todo List</h1>
-        <section class="container-area">
-            <form action="" @submit.prevent="handleAddTodo">
-                <input
-                    v-model="state.todo"
-                    class="input-area"
-                    placeholder="Todoを追加"
-                    type="text"
-                />
-            </form>
-        </section>
+        <AddTodo v-model:todo="todo" @add-todo="handleAddTodo" />
         <!-- todoListのエリア -->
-        <TodoList :todo="state.todo" :todos="state.todos" />
+        <TodoList :todo="todo" :todos="todos" />
     </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import AddTodo from './components/AddTodo.vue'
 import TodoList from './components/TodoList.vue'
-import { Todo } from './types/Todo'
 
 const state = reactive<Todo>({
     todo: '',
     todos: [],
 })
 
+type Todo = {
+    todo: string
+    todos: Array<string>
+}
+
+const todo = ref('')
+const todos = ref<Array<string>>([])
+
 const handleAddTodo = () => {
-    if (state.todo === '') {
+    if (todo.value === '') {
         return
     }
-    state.todos.push(state.todo)
-    state.todo = ''
+    todos.value.push(todo.value)
+    todo.value = ''
 }
 </script>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="container-area">
+        <form action="" @submit.prevent="handleAddTodo">
+            <input
+                :value="todo"
+                @input="onInputTodo"
+                class="input-area"
+                placeholder="Todoを追加"
+                type="text"
+            />
+        </form>
+    </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+interface Props {
+    todo: string
+}
+
+interface Emits {
+    (event: 'update:todo', todo: string): void
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits<Emits>()
+
+const handleAddTodo = (): void => {
+    emit('addTodo')
+}
+
+const onInputTodo = (event: Event): void => {
+    const element = event.target as HTMLInputElement
+    const inputTodo = String(element.value)
+    emit('update:todo', inputTodo)
+}
+</script>
